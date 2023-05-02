a2556d6cb52c8179b3589a1e85d56c5b2211672c
Denis Urbanskiy <Urbanskiy565@gmail.com>
2023-05-02 17:47:07 +0300

    remade GetElementFromBytes to accept less than 4 arguments

--- FiniteFieldsLib/BinaryFiniteField.cs
+++ FiniteFieldsLib/BinaryFiniteField.cs
@@ -14,23 +14,32 @@ public class BinaryFiniteField : FiniteField
     {
     }
 
-    public FiniteFieldElement GetElementFromBytes(byte[] bytes) //big-endian method and *my* system is little-endian
+    public FiniteFieldElement
+        GetElementFromBytes(params byte[] bytes) //big-endian method and *my* system is little-endian
     {
-        if (bytes.Length != 4)
-            throw new ArgumentException($"Array length of {bytes.Length} is not 4");
+        if (bytes.Length > 4)
+            throw new ArgumentException($"Too many bytes! {bytes.Length} bytes cannot be converted to int32");
 
         if (N % 8 != 0)
             throw new ArgumentException($"The N of {N} is not a multiple of 8");
 
         if (N / 8 < bytes.Length)
-            throw new ArgumentException($"The field is too small for that amount of bytes - N of {N} is sufficient");
+            throw new ArgumentException($"The field is too small for that amount of bytes - N of {N} is insufficient");
 
-        var number = BitConverter.ToInt32(bytes);
+        int number;
+        if (bytes.Length < 4)
+        {
+            var zeroArray = new byte[4 - bytes.Length];
+            var normalizedBytes = bytes.Concat(zeroArray).ToArray();
+            number = BitConverter.ToInt32(normalizedBytes);
+        }
+        else
+            number = BitConverter.ToInt32(bytes);
 
         var binaryString = Convert.ToString(number, 2).Reverse();
         if (BitConverter.IsLittleEndian)
             binaryString = binaryString.Reverse();
-        
+
         var polynomialCoeffs = binaryString.Select(item => item - '0').ToArray();
 
         return GetElement(polynomialCoeffs);
@@ -44,7 +53,7 @@ public class BinaryFiniteField : FiniteField
         var binaryString = BitConverter.IsLittleEndian
             ? string.Join("", element.Polynomial.Coefficients.Select(item => item.Value)).Reverse().ToArray()
             : string.Join("", element.Polynomial.Coefficients.Select(item => item.Value)).ToArray();
-        
+
         var number = Convert.ToInt32(new string(binaryString), 2);
         return BitConverter.GetBytes(number);
     }
